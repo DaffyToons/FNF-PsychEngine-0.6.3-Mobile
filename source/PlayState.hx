3f61d3fac034774e971784e56a6e507c10338c36
Lily <egzozu.be.bas@gmail.com>
2024-06-07 22:03:48 +0300

    a

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -71,8 +71,11 @@ import sys.FileSystem;
 import sys.io.File;
 #end
 
-#if VIDEOS_ALLOWED
-import vlc.MP4Handler;
+#if VIDEOS_ALLOWED 
+#if (hxCodec >= "3.0.0") import hxcodec.flixel.FlxVideo as MP4Handler;
+#elseif (hxCodec >= "2.6.1") import hxcodec.VideoHandler as MP4Handler;
+#elseif (hxCodec == "2.6.0") import VideoHandler as MP4Handler;
+#else import vlc.VideoHandler as MP4Handler; #end
 #end
 
 using StringTools;
