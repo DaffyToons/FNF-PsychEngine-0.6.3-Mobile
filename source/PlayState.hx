2aeaf71209ccf2dd4da032289051a0217c2a4ad8
Lily <egzozu.be.bas@gmail.com>
2024-06-08 16:03:28 +0300

    backwards compatibility

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -71,11 +71,8 @@ import sys.FileSystem;
 import sys.io.File;
 #end
 
-#if VIDEOS_ALLOWED 
-#if (hxCodec >= "3.0.0") import hxcodec.flixel.FlxVideo as MP4Handler;
-#elseif (hxCodec >= "2.6.1") import hxcodec.VideoHandler as MP4Handler;
-#elseif (hxCodec == "2.6.0") import VideoHandler as MP4Handler;
-#else import vlc.VideoHandler as MP4Handler; #end
+#if VIDEOS_ALLOWED
+import vlc.MP4Handler;
 #end
 
 using StringTools;
